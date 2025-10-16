663002ab813be5079e2e5e658d64cc80f4d92a50
Yernar <ernarislambek@gmail.com>
2025-10-17 03:22:01 +0500

    upd2

--- gameslist-react-app/src/GameList/GameList.jsx
+++ gameslist-react-app/src/GameList/GameList.jsx
@@ -1,10 +1,24 @@
 import React, { useState } from "react";
 import GameCard from "../GameCard/GameCard";
-import "./GameList.css"
+import "./GameList.css";
 
 const GameList = () => {
   const [games, setGames] = useState([]);
+  const [searchGame, setSearchGame] = useState("");
+  const [heading, setHeading] = useState("Click to generate list of cards");
 
+  const handleChange = (e) => {
+    setSearchGame(e.target.value);
+  };
+
+  const clearSearch = () => {
+    setSearchGame("");
+  };
+
+  const filteredGames = games.filter((card) =>
+    card.title.toLowerCase().includes(searchGame.toLowerCase())
+  );
+  
   async function generateGame() {
     let games = await fetch(`/freetogame/api/games`);
     let data = await games.json();
@@ -15,11 +29,28 @@ const GameList = () => {
 
   return (
     <div className="main-container">
-      <h1 className="main-header">Click to generate list of cards</h1>
-      <button className="main-button" onClick={generateGame}>Generate</button>
-      {games ? (
+      <h1 className="main-header">{heading}</h1>
+      <div className="main-functions-container">
+        <div className="search-container">
+          <input
+            className="search-input"
+            type="text"
+            value={searchGame}
+            onChange={handleChange}
+          />
+          <button className="search-button">Search</button>
+          <button className="clear-button" onClick={clearSearch}>
+            Clear
+          </button>
+        </div>
+        <button className="main-button" onClick={generateGame}>
+          Generate
+        </button>
+      </div>
+
+      {filteredGames ? (
         <ul>
-          {games.map((game) => (
+          {filteredGames.map((game) => (
             <GameCard
               title={game.title}
               shortDescription={game.short_description}
